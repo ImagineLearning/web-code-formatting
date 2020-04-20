3c2e94905978b3b6e4c8f4ea6844f5314ac3bbbf
Eric Gibby <eric.gibby@imaginelearning.com>
2020-04-20 11:43:04 -0600

    Updated install script to work for Angular and React configurations

--- install.js
+++ install.js
@@ -11,22 +11,34 @@ const writeFileAsync = promisify(fs.writeFile);
 
 const CWD = process.env.INIT_CWD || process.cwd();
 
-install()
+const framework = process.argv[2];
+
+if (!framework) {
+	console.error('No framework specified');
+	process.exit(1);
+}
+
+install(framework)
 	.then(() => console.log('Installation complete.'))
-	.catch(function(error) {
+	.catch(function (error) {
 		console.error('Error installing code formatting tools.', error);
 		process.exit(1);
 	});
 
-async function configureHook() {
+async function configureHook(framework) {
 	console.log('Configuring pre-commit hook...');
 	const packageFile = path.resolve(CWD, 'package.json');
 	const packageJson = JSON.parse(await readFileAsync(packageFile));
 
+	const prettierExtensions = ['ts', 'js', 'json', 'scss', 'css'];
+	if (framework === 'react') {
+		prettierExtensions.push('tsx');
+	}
+
 	// Configure lint-staged
 	const ls = Object.assign({}, packageJson['lint-staged'] || {});
-	ls['*.(htm|html)'] = ['html-beautify -r --config .jsbeautifyrc', 'git add'];
-	ls['*.(ts|js|json|scss|css)'] = ['pretty-quick --staged --config .prettierrc', 'git add'];
+	ls['*.(htm|html)'] = 'html-beautify -r --config .jsbeautifyrc';
+	ls['*.(' + prettierExtensions.join('|') + ')'] = 'pretty-quick --staged --config .prettierrc';
 	packageJson['lint-staged'] = ls;
 
 	// Configure pre-commit hook with husky
@@ -38,7 +50,8 @@ async function configureHook() {
 
 	// Configure npm scripts
 	const scripts = Object.assign({}, packageJson.scripts || {});
-	scripts['format:prettier'] = 'prettier --config .prettierrc --ignore-path "node_modules/**" --write "**/*.{ts,js,json,scss,css}"';
+	scripts['format:prettier'] =
+		'prettier --config .prettierrc --ignore-path "node_modules/**" --write "**/*.{' + prettierExtensions.join(',') + '}"';
 	scripts['format:beautify'] = 'html-beautify -r --config .jsbeautifyrc "src/**/*.{htm,html}"';
 	scripts.format = 'npm run format:prettier && npm run format:beautify';
 	packageJson.scripts = scripts;
@@ -46,7 +59,44 @@ async function configureHook() {
 	await writeFileAsync(packageFile, JSON.stringify(packageJson, null, '\t') + EOL);
 }
 
-async function configureLinting() {
+async function configureEsLint() {
+	console.log('Configuring eslint...');
+	const projectEslintFile = path.resolve(CWD, '.eslintrc');
+	let projectEslintJson = {};
+	if (fs.existsSync(projectEslintFile)) {
+		projectEslintJson = JSON.parse(await readFileAsync(projectEslintFile));
+	}
+	const eslintJson = JSON.parse(await readFileAsync(path.resolve(__dirname, 'config', 'eslint.json')));
+
+	// Merge "extends" property
+	const extendsProp = eslintJson.extends.reduce((accumulator, item) => {
+		if (accumulator.findIndex(item) < 0) {
+			return accumulator.concat([item]);
+		}
+		return accumulator;
+	}, projectEslintJson.extends || []);
+	projectEslintJson.extends = extendsProp;
+
+	// Merge "overrides" property
+	const overridesProp = eslintJson.overrides.reduce((accumulator, item) => {
+		const index = accumulator.findIndex((i) => i.files === item.files);
+		if (index < 0) {
+			return accumulator.concat([item]);
+		}
+		const rules = Object.assign({}, accumulator[index].rules, item.rules);
+		const updated = accumulator.slice();
+		updated[index].rules = rules;
+		return updated;
+	}, projectEslintJson.overrides || []);
+
+	// Merge "rules" property
+	const rules = Object.assign({}, projectEslintJson.rules || {}, eslintJson.rules);
+	projectEslintJson.rules = rules;
+
+	await writeFileAsync(projectEslintFile, JSON.stringify(projectEslintJson, null, '\t') + EOL);
+}
+
+async function configureTsLint() {
 	console.log('Configuring tslint...');
 	const projectTslintFile = path.resolve(CWD, 'tslint.json');
 	let projectTslintJson = {};
@@ -60,7 +110,7 @@ async function configureLinting() {
 		rulesDirectory = [rulesDirectory];
 	}
 	if (
-		!rulesDirectory.some(function(item) {
+		!rulesDirectory.some(function (item) {
 			return /codelyzer/.test(item);
 		})
 	) {
@@ -88,7 +138,7 @@ async function copyEditorConfig() {
 	await copyFileAsync(path.resolve(__dirname, '.editorconfig'), path.resolve(CWD, '.editorconfig'));
 }
 
-async function install() {
+async function install(framework) {
 	// Make sure we're not installing in this project
 	const packageName = '@imaginelearning/web-code-formatting';
 	const packageJson = JSON.parse(await readFileAsync(path.resolve(CWD, 'package.json')));
@@ -97,9 +147,13 @@ async function install() {
 	}
 
 	// Perform all the install steps
-	await configureLinting();
+	if (framework === 'react') {
+		await configureEsLint();
+	} else {
+		await configureTsLint();
+	}
 	await copyJsBeautifyConfig();
 	await copyPrettierConfig();
 	await copyEditorConfig();
-	await configureHook();
+	await configureHook(framework);
 }
